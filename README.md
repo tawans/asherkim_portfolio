54d769d65fe89c067087dbdba116f143c5331284
AsherKim <tawans@naver.com>
2023-10-11 05:36:20 +0900

    🎉 Asher Portfolio Site Begin

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# asherkim_portfolio
-asherkim_portfolio
+# portfolio
+
+AsherKim의 포트폴리오 사이트
---
+++ src/type.js
@@ -0,0 +1,23 @@
+'use strict';
+
+new TypeIt('.home__title--strong', {
+  loop: true,
+  speed: 100,
+}) // Dream Coder|
+  .move(-20)
+  .type('Amazing ') //Amazing |Dream Coder
+  .pause(1000)
+  .move(null, { to: 'END' }) //Amazing Dream Coder|
+  .delete() //|
+  .type('Flutter Developer') //Front-end Engineer|
+  .pause(1000)
+  .move(-10) //Front-end| Engineer
+  .delete(10) //| Engineer
+  .type('iOS') //Back-end| Engineer
+  .pause(1000)
+  .delete(9) //| Engineer
+  .type('Front-end') //Full-stack| Engineer
+  .pause(1000)
+  .move(10) //Full-stack Engineer|
+  .delete()
+  .go();
